1d489a78ba9a4ca8195cf20bb049e857b3edf763
mounika ghanta <mounikaghanta04@gmail.com>
2024-12-16 23:24:52 +0530

    fix(app): remove base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sip-calculator',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
